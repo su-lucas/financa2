00a4e50387ec52920457e97c2201c6f185fbe5fc
Lucas Guedes <lucasdev203@gmail.com>
2026-09-12 11:52:24 -0300

    adicionei o usr admin nos admin de cada app

--- backend/categorias/admin.py
+++ backend/categorias/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from backend.categorias.models import Categoria
+
 # Register your models here.
+#colocando o administrador para poder ver os modelos no admin do django
+admin.site.register(Categoria)
